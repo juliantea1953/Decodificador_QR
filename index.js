0b6c1e27c31aa0a2b60279c8ce22e547a1441ddc
davidtea <davidtea1953@gmail.com>
2022-10-25 15:37:17 -0500

    se crea codigo base para decodificar QR

---
+++ index.js
@@ -0,0 +1,21 @@
+var fs = require("fs")
+var QrCode = require('qrcode-reader');
+var Jimp = require("jimp");
+
+var buffer = fs.readFileSync(__dirname + '/barcode.png');
+Jimp.read(buffer, function(err, image) {
+    if (err) {
+        console.error(err);
+        // TODO handle error
+    }
+    var qr = new QrCode();
+    qr.callback = function(err, value) {
+        if (err) {
+            console.error(err);
+            // TODO handle error
+        }
+        console.log(value.result);
+        console.log(value);
+    };
+    qr.decode(image.bitmap);
+});
